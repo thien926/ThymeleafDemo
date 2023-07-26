ab9a6f20851b0e4eb753be3a5bee2e2231d71587
Thien Nguyen <thien926@users.noreply.github.com>
2023-07-26 23:20:33 +0700

    Registration and Login with Spring Boot, Spring Security, Thymeleaf, Hibernate and MySQL - PART 2

---
+++ project/registration-login-spring-boot-security-thymeleaf/src/main/java/net/javaguides/springboot/model/User.java
@@ -0,0 +1,50 @@
+package net.javaguides.springboot.model;
+
+import java.util.Collection;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "user", uniqueConstraints = @UniqueConstraint(columnNames = "email"))
+@Entity
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "first_name")
+	private String firstName;
+	
+	@Column(name = "last_name")
+	private String lastName;
+	
+	@Column(name = "email")
+	private String email;
+	
+	@Column(name = "password")
+	private String password;
+	
+	@ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+	@JoinTable(name = "users_roles"
+	, joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id")
+	, inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+	private Collection<Role> roles;
+}
